efebd7438c3bac9129a78d4fd8f9a81c9142f93a
nithyage <nithya.niranjan@geappliances.com>
2024-08-26 18:32:21 +0530

    updating damage img chat

--- src/components/DamageImageChat.js
+++ src/components/DamageImageChat.js
@@ -71,7 +71,8 @@ const DamageImageChat = () => {
         const response = await axios.get(config.BASE_URL, {
           headers: {
             'content-type': 'application/json',
-            'api-key': 'f073d13164ce42a5a6ac3b5cfd39300277'
+            'api-key': 'f073d13164ce42a5a6ac3b5cfd39300277 '
+
 
           },
           params: {
